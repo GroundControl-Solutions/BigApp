820f1c34b8a210e32ad09c38eed27c589910d770
Jeremy Agostino <jeremy@groundctl.com>
2016-05-16 16:46:59 -0400

    Project files

---
+++ BigApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BigApp
+//
+//  Created by Jeremy Agostino on 5/16/16.
+//  Copyright © 2016 GroundControl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
